2bc323782b1f9207e40394395ff764aa497c8b02
Andreza <andrezanascimentodesousa@gmail.com>
2021-04-25 15:56:30 -0300

    commit

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -49,6 +49,11 @@ sr.reveal('.skills_text',{delay: 200})
 sr.reveal('.skills_data',{interval: 200})
 sr.reveal('.skills_img',{delay: 400})
 
+//Scroll courses
+sr.reveal('.curso_subtitle',{})
+sr.reveal('.curso_text',{delay: 200})
+sr.reveal('.curso_data',{interval: 200})
+sr.reveal('.curso_img', {delay: 400})
 
 //Scroll work
 sr.reveal('.work_img',{interval: 200})
